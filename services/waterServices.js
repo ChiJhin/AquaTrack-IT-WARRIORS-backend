5173e77ad0a779668f3d7f3dd5afe34839f69215
AntonSizov <sizov.anton.v@gmail.com>
2024-05-10 00:12:06 +0300

    water req v1

--- services/waterServices.js
+++ services/waterServices.js
@@ -1,6 +1,24 @@
 import { Water } from "../models/waterModel.js"
 import HttpError from '../helpers/HttpError.js'
 
+export const localDate = () => {
+  const milliseconds = Date.now();
+  const date = new Date(milliseconds);
+  
+  return date.toLocaleDateString();
+};
+
+export const localTime = () => {
+  const milliseconds = Date.now();
+  const time = new Date(milliseconds);
+  
+  const timeString = time.toLocaleTimeString();
+  const parts = timeString.split(':');
+  parts.pop();
+
+  return parts.join(":");
+};
+
 export const addWaterDataService = async (data) => {
   try {
     const addData = await Water.create(data);
@@ -14,3 +32,43 @@ export const addWaterDataService = async (data) => {
     error.message
   };
 };
+
+export const updateWaterDataService = async (id, newData) => {
+  const updatedData = Water.findOneAndUpdate({
+    _id: id
+  },
+    newData,
+    { new: true });
+  
+  if (!updatedData) {
+    throw HttpError(404, 'Not found')
+  };
+
+  return updatedData;
+};
+
+export const deleteWaterDateService = async (id) => {
+  try {
+    const deletedData = await Water.findOneAndDelete({ _id: id });
+
+    if (!deletedData) {
+      throw HttpError(404, "Not found")
+    }
+
+    return deletedData;
+  } catch (error) {
+    
+  }
+};
+
+export const daylyWaterData = async () => {
+  try {
+    const daylyWater = Water.find();
+
+    if (!daylyWater) throw HttpError(404, 'Not found');
+
+    return daylyWater;
+  } catch (error) {
+    throw error.message
+  }
+}
