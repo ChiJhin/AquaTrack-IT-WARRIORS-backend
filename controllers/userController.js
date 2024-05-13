83304de579977c0fa17d9b119f4732eb6e711444
irensmile <curkan.ira@gmail.com>
2024-05-13 00:38:10 -0400

    1. Make auth token expirable 2. Return auth token on register route, so user does not have to log in after the registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,42 +1,67 @@
-import { loginDataService, logoutUserDataService, regenerateTokenDataService, registerDataService, updateUserUserDataService } from "../services/userServices.js";
+import {
+  loginDataService,
+  logoutUserDataService,
+  regenerateTokenDataService,
+  registerDataService,
+  updateUserUserDataService,
+} from "../services/userServices.js";
 
 export const register = async (req, res, next) => {
-    const { email, name, password } = req.body;
-    const newUser = await registerDataService(email, name, password)
+  const { email, name, password } = req.body;
+  const newUser = await registerDataService(email, name, password);
 
-    res.status(201).json({
-      user: { email: newUser.email },
-    });
-  };
+  res.status(201).json({
+    user: { email: newUser.email },
+    token: newUser.token,
+  });
+};
 
 export const login = async (req, res) => {
-    const { email, password } = req.body;
-    const generatedToken = await loginDataService(email, password);
-    res.status(200).json({
-        token: generatedToken,
-        user: { email },
-      });
+  const { email, password } = req.body;
+  const generatedToken = await loginDataService(email, password);
+  res.status(200).json({
+    token: generatedToken,
+    user: { email },
+  });
 };
 
 export const logout = async (req, res) => {
-    await logoutUserDataService(req.user._id)
-    res.status(204).json();
+  await logoutUserDataService(req.user._id);
+  res.status(204).json();
 };
 
 export const current = async (req, res) => {
-    const { _id, email, name, gender, weight, dailyActivityTime, dailyWaterNorm, avatarURL } = req.user;
-    res.json({ _id, email, name, gender, weight, dailyActivityTime, dailyWaterNorm, avatarURL });
+  const {
+    _id,
+    email,
+    name,
+    gender,
+    weight,
+    dailyActivityTime,
+    dailyWaterNorm,
+    avatarURL,
+  } = req.user;
+  res.json({
+    _id,
+    email,
+    name,
+    gender,
+    weight,
+    dailyActivityTime,
+    dailyWaterNorm,
+    avatarURL,
+  });
 };
 
 export const updateUser = async (req, res) => {
-    const updatedObject = await updateUserUserDataService(req.user._id, req.body);
-    res.json(updatedObject)
+  const updatedObject = await updateUserUserDataService(req.user._id, req.body);
+  res.json(updatedObject);
 };
 
 export const regenerateToken = async (req, res) => {
-    const generatedToken = await regenerateTokenDataService(req.user._id);
-    res.status(200).json({
-        token: generatedToken,
-        user: { email: req.user.email },
-      });
+  const generatedToken = await regenerateTokenDataService(req.user._id);
+  res.status(200).json({
+    token: generatedToken,
+    user: { email: req.user.email },
+  });
 };
--- services/userServices.js
+++ services/userServices.js
@@ -1,61 +1,65 @@
 import bcrypt from "bcrypt";
-import gravatar from "gravatar";
 import jwt from "jsonwebtoken";
 
-import { User } from "../models/userModel.js"
-import HttpError from '../helpers/HttpError.js'
+import { User } from "../models/userModel.js";
+import HttpError from "../helpers/HttpError.js";
 
 export const registerDataService = async (email, name, password) => {
-    if ((await User.findOne({ email })) !== null) {
-        throw HttpError(409, "Email in use");
-      }
-    
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const avatarURL = gravatar.url(email);
-    
-      return await User.create({
-        password: hashedPassword,
-        email: email,
-        name: name,
-        avatarURL //verificationToken = nanoid() // Additional task
-      });
-}
-export const loginDataService = async (email, password) => {
-    const foundUser = await User.findOne({ email });
+  if ((await User.findOne({ email })) !== null) {
+    throw HttpError(409, "Email in use");
+  }
 
-    if (!foundUser)
-      throw HttpError(401, "Email or password is wrong");
-  
-    const isPasswordMatching = await bcrypt.compare(password, foundUser.password);
-    if (!isPasswordMatching)
-      throw HttpError(401, "Email or password is wrong");
-  
-    const payload = { id: foundUser._id };
-    const generatedToken = jwt.sign(payload, process.env.SECRET);
-  
-    await User.findByIdAndUpdate(
-        foundUser.id, {token: generatedToken});
-    return generatedToken;
-}
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({
+    email,
+    name,
+    password: hashedPassword,
+  });
+
+  const payload = { id: newUser._id };
+  const generatedToken = jwt.sign(payload, process.env.SECRET, {
+    expiresIn: process.env.AUTH_EXPIRATION,
+  });
+
+  return await User.findByIdAndUpdate(
+    newUser._id,
+    { token: generatedToken },
+    { new: true }
+  );
+};
+export const loginDataService = async (email, password) => {
+  const foundUser = await User.findOne({ email });
+
+  if (!foundUser) throw HttpError(401, "Email or password is wrong");
+
+  const isPasswordMatching = await bcrypt.compare(password, foundUser.password);
+  if (!isPasswordMatching) throw HttpError(401, "Email or password is wrong");
+
+  const payload = { id: foundUser._id };
+  const generatedToken = jwt.sign(payload, process.env.SECRET, {
+    expiresIn: process.env.AUTH_EXPIRATION,
+  });
+
+  await User.findByIdAndUpdate(foundUser.id, { token: generatedToken });
+  return generatedToken;
+};
 
 export const logoutUserDataService = async (userId) => {
-    await User.findByIdAndUpdate(
-        { _id: userId},
-        { token: null }
-      );
-}
+  await User.findByIdAndUpdate({ _id: userId }, { token: null });
+};
 
 export const updateUserUserDataService = async (userId, params) => {
-    const foundUser = User.findById(userId);
-    if (!foundUser)
-        throw HttpError(401, 'User not found');
-    return await User.findByIdAndUpdate(userId, params, { new: true });
-}
+  const foundUser = User.findById(userId);
+  if (!foundUser) throw HttpError(401, "User not found");
+  return await User.findByIdAndUpdate(userId, params, { new: true });
+};
 
 export const regenerateTokenDataService = async (userId) => {
-    const payload = { id: userId };
-    const generatedToken = jwt.sign(payload, process.env.SECRET);
-    await User.findByIdAndUpdate(
-        userId, {token: generatedToken});
-    return generatedToken;
-}
+  const payload = { id: userId };
+  const generatedToken = jwt.sign(payload, process.env.SECRET, {
+    expiresIn: process.env.AUTH_EXPIRATION,
+  });
+  await User.findByIdAndUpdate(userId, { token: generatedToken });
+  return generatedToken;
+};
